47b7fccd83bad2c02bce20b022c42a806122faba
RubyJangra <rubyjangra99@gmail.com>
2024-02-20 22:37:51 +0530

    firstgithub

---
+++ firstgithub/src/firstgithub/haiii.java
@@ -0,0 +1,11 @@
+package firstgithub;
+
+public class haiii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
---
+++ firstgithub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author romit
+ *
+ */
+module firstgithub {
+}
